79ecc5c2aff935b7fcd2ce214451f345d65151cd
Gregg Fine <bbeez8500@gmail.com>
2018-07-18 11:37:53 -0400

    Create Button Component to Play Sound

--- src/components/Play_Rand_Int.js
+++ src/components/Play_Rand_Int.js
@@ -1,11 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Play_Rand_Int = () => {
-    return (
-        <button>
-            Play Random Interval
-        </button>
-    )
+const context = new (window.AudioContext || window.webkitAudioContext)();
+
+
+class Play_Rand_Int extends Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+            context: context,
+            oscType: 'sine',
+        }
+
+        this.init = this.init.bind(this)
+        this.play = this.play.bind(this)
+        this.stop = this.stop.bind(this)
+
+    }
+    init() {
+        this.oscillator = this.state.context.createOscillator();
+        this.gainNode = this.state.context.createGain();
+    
+        this.oscillator.connect(this.gainNode);
+        this.gainNode.connect(this.state.context.destination);
+        this.oscillator.type = this.state.oscType;
+      }
+    
+      play(value, time, gainLevel) {
+        this.init();
+
+        this.oscillator.frequency.value = value;
+        this.gainNode.gain.value = gainLevel;
+        this.oscillator.start(time);
+        this.stop(time);
+      }
+    
+      stop(time) {
+        this.gainNode.gain.exponentialRampToValueAtTime(0.100, time + 1);
+        this.oscillator.stop(time + 1);
+      }
+
+    render() {
+        return (
+            <div>
+                <button onClick={this.play(440, 0, 1.0)}>Play Random Interval</button>
+            </div>
+        )
+    }
 }
 
 export default Play_Rand_Int;
